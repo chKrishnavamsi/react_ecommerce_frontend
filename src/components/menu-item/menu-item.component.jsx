a4ff4c1f5d0b63306e60fac4269977ebdfe58853
chKrishnavamsi <chimatakrishnavamsi@gmail.com>
2021-05-01 02:35:58 +0530

    Enter

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,9 @@
  import React from 'react';
- import {withRouter} from 'react-router-dom'
-
+ import {withRouter} from 'react-router-dom';
  import './menu-item.styles.scss'
 
 
- const MenuItem = ({ title, imageUrl, size ,linkUrl,match}) => (
+ const MenuItem = ({ title, imageUrl, size ,linkUrl,match,history}) => (
    <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
      <div
        className="background-image"
@@ -14,7 +13,7 @@
        }}
      />
      <div className="content">
-       <h1 className="title">{title}</h1>
+       <h1 className="title">{title.toUpperCase()}</h1>
        <span className="subtitle">SHOP NOW</span>
      </div>
    </div>
